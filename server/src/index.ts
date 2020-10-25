e9e4f454dde634d38ec1e48ddb00b26d2c5d2389
Ernest Wong <sudo@ernestwong.nz>
2020-10-26 11:06:08 +1300

    Wait before connecting to vnc

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,17 +4,19 @@ import nocache from 'nocache'
 import html from './html'
 import Session from './session'
 
-const session = new Session()
+setTimeout(() => {
+  const session = new Session()
 
-const app = express()
-app.use(nocache())
+  const app = express()
+  app.use(nocache())
 
-app.get('/', (_: express.Request, response: express.Response) => {
-  response.send(html)
-})
+  app.get('/', (_: express.Request, response: express.Response) => {
+    response.send(html)
+  })
 
-app.use(session.getRouter())
+  app.use(session.getRouter())
 
-app.listen(3000, () => {
-  console.log('Ready')
-})
+  app.listen(3000, () => {
+    console.log('Ready')
+  })
+}, 10000)
